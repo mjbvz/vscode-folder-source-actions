e7bb047cad5378b9f97abf4e29ec9f9e1b1058ee
Matt Bierner <matb@microsoft.com>
2019-03-07 17:37:25 -0800

    Adding fix all in folder action

--- src/extension.ts
+++ src/extension.ts
@@ -4,24 +4,40 @@ const fakeWholeDocumentRange = new vscode.Range(0, 0, 99999, 0);
 
 export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(
-        vscode.commands.registerCommand('folderSourceActions.organizeImports', organizeImportsInDirectory));
+        vscode.commands.registerCommand('folderSourceActions.organizeImports',
+            createFolderSourceAction(
+                vscode.CodeActionKind.SourceOrganizeImports,
+                "Organizing Imports in Folder"
+            )));
+
+    context.subscriptions.push(
+        vscode.commands.registerCommand('folderSourceActions.fixAll',
+            createFolderSourceAction(
+                vscode.CodeActionKind.SourceFixAll,
+                "Fixing All in Folder"
+            )));
 }
 
-async function organizeImportsInDirectory(dir: vscode.Uri) {
-    vscode.window.withProgress(
-        {
-            location: vscode.ProgressLocation.Window,
-            title: "Organizing Imports in Folder"
-        },
-        async () => {
-            const files = await getPotentialFilesToOrganize(dir);
-            return Promise.all(files
-                .map(getOrganizeImportsActionForFile)
-                .map(action => action.then(tryApplyCodeAction)));
-        });
+function createFolderSourceAction(
+    kind: vscode.CodeActionKind,
+    progressLabel: string,
+) {
+    return function (dir: vscode.Uri) {
+        return vscode.window.withProgress(
+            {
+                location: vscode.ProgressLocation.Window,
+                title: progressLabel
+            },
+            async () => {
+                const files = await getPotentialFilesForSourceAction(dir);
+                return Promise.all(files
+                    .map(file => getSourceActionForFile(kind, file))
+                    .map(action => action.then(tryApplyCodeAction)));
+            });
+    }
 }
 
-async function getPotentialFilesToOrganize(
+async function getPotentialFilesForSourceAction(
     dir: vscode.Uri
 ): Promise<ReadonlyArray<vscode.Uri>> {
     return vscode.workspace.findFiles(
@@ -29,29 +45,32 @@ async function getPotentialFilesToOrganize(
         '**/node_modules/**');
 }
 
-async function getOrganizeImportsActionForFile(
+async function getSourceActionForFile(
+    kind: vscode.CodeActionKind,
     file: vscode.Uri
 ): Promise<vscode.CodeAction | undefined> {
     try {
-        const allActions = (await getAllCodeActionsForFile(file)) || [];
-        return allActions.find(isOrganizeImportsAction);
+        const allActions = (await getAllCodeActionsForFile(file, kind)) || [];
+        return allActions.find(actionFilter(kind));
     } catch  {
         // noop 
     }
     return undefined;
 }
 
-function getAllCodeActionsForFile(file: vscode.Uri): Thenable<ReadonlyArray<vscode.CodeAction> | undefined> {
+function getAllCodeActionsForFile(file: vscode.Uri, kind: vscode.CodeActionKind): Thenable<ReadonlyArray<vscode.CodeAction> | undefined> {
     // We need make sure VS Code knows about the file before trying to request code actions
     return vscode.workspace.openTextDocument(file).then(doc =>
         vscode.commands.executeCommand('vscode.executeCodeActionProvider',
             file,
             fakeWholeDocumentRange,
-            vscode.CodeActionKind.SourceOrganizeImports));
+            kind));
 }
 
-function isOrganizeImportsAction(action: vscode.CodeAction): boolean {
-    return action && !!action.kind && vscode.CodeActionKind.SourceOrganizeImports.contains(action.kind);
+function actionFilter(targetKind: vscode.CodeActionKind) {
+    return function (action: vscode.CodeAction): boolean {
+        return action && !!action.kind && targetKind.contains(action.kind);
+    };
 }
 
 async function tryApplyCodeAction(
